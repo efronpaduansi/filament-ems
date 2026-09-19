2980166b2ffe56a274ecc248ee882759980820d9
efronpaduansi <eufrondpaduansi@gmail.com>
2026-09-19 12:22:51 +0700

    customize notification

--- app/Filament/Resources/DepartmentResource/Pages/CreateDepartment.php
+++ app/Filament/Resources/DepartmentResource/Pages/CreateDepartment.php
@@ -4,9 +4,21 @@ namespace App\Filament\Resources\DepartmentResource\Pages;
 
 use App\Filament\Resources\DepartmentResource;
 use Filament\Actions;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateDepartment extends CreateRecord
 {
     protected static string $resource = DepartmentResource::class;
+
+//    protected function getCreatedNotificationTitle(): ?string
+//    {
+//        return 'Department Created';
+//    }
+
+    //Modifikasi pesan notifikasi
+    protected function getCreatedNotification(): ?Notification
+    {
+        return Notification::make()->success()->title('Department Created')->body('The Department created successfully.');
+    }
 }
